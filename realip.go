d98f6e34fa371b7281f34ba15593cf8ad823bb86
yunjieguo <yunjieguo@tencent.com>
2021-12-27 19:22:53 +0800

    fix

--- realip.go
+++ realip.go
@@ -51,36 +51,34 @@ func isPrivateAddress(address string) (bool, error) {
 
 // FromRequest return client's real public IP address from http request headers.
 func FromRequest(r *http.Request) string {
-	// Fetch header value
+	// Fetch x-real-ip value
 	xRealIP := r.Header.Get("X-Real-Ip")
+	if xRealIP != "" {
+		return xRealIP
+	}
+
 	xForwardedFor := r.Header.Get("X-Forwarded-For")
-
-	// If both empty, return IP from remote address
-	if xRealIP == "" && xForwardedFor == "" {
-		var remoteIP string
-
-		// If there are colon in remote address, remove the port number
-		// otherwise, return remote address as is
-		if strings.ContainsRune(r.RemoteAddr, ':') {
-			remoteIP, _, _ = net.SplitHostPort(r.RemoteAddr)
-		} else {
-			remoteIP = r.RemoteAddr
-		}
-
-		return remoteIP
-	}
-
-	// Check list of IP in X-Forwarded-For and return the first global address
-	for _, address := range strings.Split(xForwardedFor, ",") {
-		address = strings.TrimSpace(address)
-		isPrivate, err := isPrivateAddress(address)
-		if !isPrivate && err == nil {
-			return address
+	if xForwardedFor != "" {
+		// Check list of IP in X-Forwarded-For and return the first global address
+		for _, address := range strings.Split(xForwardedFor, ",") {
+			address = strings.TrimSpace(address)
+			isPrivate, err := isPrivateAddress(address)
+			if !isPrivate && err == nil {
+				return address
+			}
 		}
 	}
 
-	// If nothing succeed, return X-Real-IP
-	return xRealIP
+	var remoteIP string
+	// If there are colon in remote address, remove the port number
+	// otherwise, return remote address as is
+	if strings.ContainsRune(r.RemoteAddr, ':') {
+		remoteIP, _, _ = net.SplitHostPort(r.RemoteAddr)
+	} else {
+		remoteIP = r.RemoteAddr
+	}
+
+	return remoteIP
 }
 
 // RealIP is depreciated, use FromRequest instead
